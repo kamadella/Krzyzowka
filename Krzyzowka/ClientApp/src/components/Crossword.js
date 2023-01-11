9bc47849550bc6676570793952cc724e1ac606cc
Kamila Adamska <kamilaaadamska@gmail.com>
2023-01-11 16:00:39 +0100

    modify crossword

--- Krzyzowka/ClientApp/src/components/Crossword.js
+++ Krzyzowka/ClientApp/src/components/Crossword.js
@@ -1,31 +1,98 @@
 import React, { Component } from 'react';
+import Grid from "./Grid";
 
 export class Crossword extends Component {
-  static displayName = Crossword.name;
-
   constructor(props) {
     super(props);
-    this.state = { currentCount: 0 };
-    this.incrementCounter = this.incrementCounter.bind(this);
-  }
 
-  incrementCounter() {
-    this.setState({
-      currentCount: this.state.currentCount + 1
-    });
-  }
+    this.state = {
+        data: {
+            height: 13,
+            width: 13,
+            wordList: [
+                {
+                    word: "Word",
+                    orientation: "down",
+                    x: 2,
+                    y: 1,
+                    length: 5
+                },
+                {
+                    word: "consolidate",
+                    orientation: "across",
+                    x: 1,
+                    y: 2,
+                    length: 5
+                },
+                {
+                    word: "shift",
+                    orientation: "down",
+                    x: 4,
+                    y: 2,
+                    length: 5
+                },
+                {
+                    word: "ice",
+                    orientation: "across",
+                    x: 4,
+                    y: 4,
+                    length: 5
+                },
+                {
+                    word: "truce",
+                    orientation: "across",
+                    x: 4,
+                    y: 6,
+                    length: 5
+                },
+                {
+                    word: "relish",
+                    orientation: "down",
+                    x: 9,
+                    y: 5,
+                    length: 5
+                }
+            ],
+            clues: [ 
+                "Co to świat?",
+                "Co to świat?", 
+                "Co to świat?", 
+                "Co to świat?", 
+                "Co to świat?", 
+                "Co to świat?" 
+            ]
+        }
+    };
+}
 
-  render() {
-    return (
-      <div>
-        <h1>Counter</h1>
+checkAnswers = () => {
+    console.log("check answers");
+};
 
-        <p>This is a simple example of a React component.</p>
 
-        <p aria-live="polite">Current count: <strong>{this.state.currentCount}</strong></p>
-
-        <button className="btn btn-primary" onClick={this.incrementCounter}>Increment</button>
-      </div>
-    );
-  }
+render() {
+    //return (
+    //    <div>
+    //        <p>{this.state.data.wordList.length}</p>
+    //        <Grid data={this.state.data}></Grid>
+    //    </div>
+    //);
+    if (this.state.data.wordList.length > 0) {
+        return (
+            <React.Fragment>
+                <Grid data={this.state.data}></Grid>
+                {this.state.data.clues.map((clue) => {
+                    return (
+                        <li key={clue} onClick={this.checkAnswers}>
+                            {clue}
+                        </li>
+                    );
+                })}
+                <button onClick={this.checkAnswers}>Check answers</button>
+            </React.Fragment>
+        );
+    } else {
+        return <p>Loading...</p>;
+    }
+}
 }
